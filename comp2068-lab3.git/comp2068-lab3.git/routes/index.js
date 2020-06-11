fda796bf1acd18a892f38ccf25f01315cbdaa4c3
Yhames1978 <54914229+Yhames1978@users.noreply.github.com>
2020-06-11 17:58:21 -0400

    Final Copy

--- comp2068-lab3.git/comp2068-lab3.git/routes/index.js
+++ comp2068-lab3.git/comp2068-lab3.git/routes/index.js
@@ -4,7 +4,45 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    var name = "The Kaczorowski Family"
+
+    res.render('index', { name: name });
 });
+router.get('/henry/', function (req, res) {
+    var name = "Henry"
+    
+    res.render('henry', { name: name });
+});
+router.get('/ursula/', function (req, res) {
+    var name = "Ursula"
+
+    res.render('ursula', { name: name });
+});
+router.get('/stanley/', function (req, res) {
+    var name = "Stanley"
+
+    res.render('stanley', { name: name });
+});
+router.get('/julie/', function (req, res) {
+    var name = "Julie"
+
+    res.render('julie', { name: name });
+});
+router.get('/yolanda/', function (req, res) {
+    var name = "Yolanda"
+
+    res.render('yolanda', { name: name });
+});
+router.get('/mike/', function (req, res) {
+    var name = "Mike"
+
+    res.render('mike', { name: name });
+});
+router.get('/matthew/', function (req, res) {
+    var name = "Matthew"
+
+    res.render('matthew', { name: name });
+});
+
 
 module.exports = router;
